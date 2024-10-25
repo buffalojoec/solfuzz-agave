483dfea093e674c79d088ea99fad7b609931508e
Joe Caulfield <joe.caulfield@anza.xyz>
2024-10-25 06:17:24 +0000

    update `fetch_programs.sh` script

--- scripts/fetch_program.sh
+++ scripts/fetch_program.sh
@@ -5,14 +5,20 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-mkdir -p programs
+BPF_PROGRAMS_DIR="bpf_programs"
+BPF_PROGRAMS_OUT_DIR="$BPF_PROGRAMS_DIR/lib"
 
-if [ -d "programs/$1" ]; then
+mkdir -p $BPF_PROGRAMS_DIR
+mkdir -p $BPF_PROGRAMS_OUT_DIR
+
+if [ -d "$BPF_PROGRAMS_DIR/$1" ]; then
     echo "Updating program $1...";
-    (cd programs/$1 && git fetch && git pull);
+    (cd $BPF_PROGRAMS_DIR/$1 && git fetch && git pull);
 else
     echo "Cloning program $1...";
-    git clone https://github.com/solana-program/$1 programs/$1;
+    git clone https://github.com/solana-program/$1 $BPF_PROGRAMS_DIR/$1;
 fi
 
-cargo build-sbf --manifest-path=programs/$1/program/Cargo.toml --sbf-out-dir programs
+cargo build-sbf --manifest-path=$BPF_PROGRAMS_DIR/$1/program/Cargo.toml \
+    --features bpf-entrypoint \
+    --sbf-out-dir $BPF_PROGRAMS_OUT_DIR
